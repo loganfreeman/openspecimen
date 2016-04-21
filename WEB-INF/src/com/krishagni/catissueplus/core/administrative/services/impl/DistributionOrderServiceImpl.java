80bd919912703e9655d892a5f02a809c0edc2b85
Chetan Khilosiya <chetan.khilosiya@krishagni.com>
2016-04-21 11:21:06 +0530

    OPSMN-3006: Removed the stream join method, and used string list directly in error message.

--- WEB-INF/src/com/krishagni/catissueplus/core/administrative/services/impl/DistributionOrderServiceImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/administrative/services/impl/DistributionOrderServiceImpl.java
@@ -363,7 +363,7 @@ public class DistributionOrderServiceImpl implements DistributionOrderService, O
 
 		if (specimens.size() != specimenLabels.size()) {
 			List<String> labels = (List<String>) CollectionUtils.subtract(specimenLabels,  Utility.<List<String>>collect(specimens, "label"));
-			ose.addError(DistributionOrderErrorCode.SPECIMEN_DOES_NOT_EXIST, labels.stream().collect(Collectors.joining(", ")));
+			ose.addError(DistributionOrderErrorCode.SPECIMEN_DOES_NOT_EXIST, labels);
 			return null;
 		}
 		
