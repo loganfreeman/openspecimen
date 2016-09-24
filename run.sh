fc8565048275c02fd11f4c2546674b4675e4598e
shanhong cheng <shnahongcheng@gmail.com>
2016-09-23 22:47:07 -0600

    add tomcat context through Dockerfile

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ if [ ! -f /.tomcat_admin_created ]; then
     /create_tomcat_admin_user.sh
 fi
 
+if [ ! -f /.tomcat_context_created ]; then
+    /create_tomcat_context.sh
+fi
+
 exec ${CATALINA_HOME}/bin/catalina.sh run
