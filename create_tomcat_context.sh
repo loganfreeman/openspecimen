caa91db9b7364ccd2d016a79ca2b4e6236a3bd0c
shanhong cheng <shnahongcheng@gmail.com>
2016-09-24 00:10:55 -0600

    db

--- create_tomcat_context.sh
+++ create_tomcat_context.sh
@@ -10,7 +10,7 @@ sed -i -r 's/<\/Context>//' ${CATALINA_HOME}/conf/context.xml
 echo '<Resource name="jdbc/openspecimen" auth="Container" type="javax.sql.DataSource"' >> ${CATALINA_HOME}/conf/context.xml
 echo 'maxTotal="100" maxIdle="30" maxWaitMillis="10000"' >> ${CATALINA_HOME}/conf/context.xml
 echo 'username="root" password="pass22" driverClassName="com.mysql.jdbc.Driver"' >> ${CATALINA_HOME}/conf/context.xml
-echo 'url="jdbc:mysql://172.17.0.1:3306/osdb"/>' >> ${CATALINA_HOME}/conf/context.xml
+echo 'url="jdbc:mysql://db:3306/osdb"/>' >> ${CATALINA_HOME}/conf/context.xml
 echo '</Context>' >> ${CATALINA_HOME}/conf/context.xml
 echo "=> Done!"
 touch /.tomcat_context_created
